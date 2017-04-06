158573c5c7c9be169accbca01a4a2bf40cfe9d5d
unknown <>
2017-04-06 17:37:56 +0900

    First Upload

--- 3-3.py
+++ 3-3.py
@@ -1,11 +1,12 @@
 #3주차 과제 3
 
 
-def infolist (filename):
-        f = open ("C:/Users/kpic1/Desktop/포리프/파이썬반/과제/3주차/3번과제 텍스트 파일/%s.txt" %filename , 'r')
-        data = f.read()
-        f = open("C:/Users/kpic1/Desktop/포리프/파이썬반/과제/3주차/3번과제 텍스트 파일/result.txt" , 'a')
-        f.write(data)
-        f.write("\n")
-        f.close()
-        f.close()
+def infolist (*arg):
+        for i in arg :
+                f = open ("C:/Users/kpic1/Desktop/포리프/파이썬반/과제/3주차/3번과제 텍스트 파일/%s.txt" %i , 'r')
+                data = f.read()
+                f = open("C:/Users/kpic1/Desktop/포리프/파이썬반/과제/3주차/3번과제 텍스트 파일/result.txt" , 'a')
+                f.write(data)
+                f.write("\n")
+                f.close()
+                f.close()
